de9597039b1c121266bca0d1869676f98c256a16
Jorge Escobar <jorge@fromzero.io>
2018-05-09 19:37:40 -0400

    removing uniqueness on RedirectLog code

--- reftrak_app/models.py
+++ reftrak_app/models.py
@@ -2,25 +2,9 @@ from datetime import datetime
 
 from application import db
 
-class RedirectLog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    ip = db.Column(db.String(64), index=True)
-    referrer = db.Column(db.String(255), index=True)
-    code = db.Column(db.String(64), unique=True, index=True)
-    timestamp = db.Column(db.DateTime(), index=True)
-
-    def __init__(self, ip, referrer, code, timestamp=datetime.utcnow()):
-        self.ip = ip
-        self.referrer = referrer
-        self.code = code
-        self.timestamp = timestamp
-
-    def __repr__(self):
-        return '<RedirectLog %s>' % self.id
-
 class RedirectList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(64), index=True)
+    code = db.Column(db.String(64), unique=True, index=True)
     url = db.Column(db.String(255), index=True)
     description = db.Column(db.String(64), index=True)
 
@@ -31,3 +15,19 @@ class RedirectList(db.Model):
 
     def __repr__(self):
         return '<Redirect %s>' % self.id
+
+class RedirectLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(64), index=True)
+    referrer = db.Column(db.String(255), index=True)
+    code = db.Column(db.String(64), index=True)
+    timestamp = db.Column(db.DateTime(), index=True)
+
+    def __init__(self, ip, referrer, code, timestamp=datetime.utcnow()):
+        self.ip = ip
+        self.referrer = referrer
+        self.code = code
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return '<RedirectLog %s>' % self.id
